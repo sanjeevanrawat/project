90acdacf3d1dd9f259cc08807f1f6b4478646eb5
sanjeevan_rawat <sanjeevanrawat1199@gmail.com>
2021-05-16 17:41:51 +0530

    16 may excel

--- Browser_Excel_Sheet/script.js
+++ Browser_Excel_Sheet/script.js
@@ -1,5 +1,7 @@
 let iconContainer = document.querySelector(".icon-container");
-let sheetList = document.querySelector(".sheet-list")
+let sheetList = document.querySelector(".sheet-list");
+let firstSheet = document.querySelector('.sheet-list');
+firstSheet.addEventListener('click', handleClick);
 iconContainer.addEventListener('click', function(){
     console.log('df');
     //create element
@@ -12,6 +14,7 @@ iconContainer.addEventListener('click', function(){
 
     //set class
     newSheet.setAttribute('class', 'sheet');
+    newSheet.classList.add('active');
 
     //set attribute
     newSheet.setAttribute('idx', Number(idx)+1);
@@ -21,6 +24,46 @@ iconContainer.addEventListener('click', function(){
 
     //append it
     sheetList.appendChild(newSheet);
+
+    //new sheet is added to it, so we want all sheets at this current moment
+    allSheet = document.querySelectorAll('.sheet');
+
+    setLastActive(allSheet);
+    // //to remove active class from all sheets
+    // for(let i = 0; i < allSheet.length; i++){
+    //     allSheet[i].classList.remove("active");
+    // }
+
+    // //adding active class to last sheet 
+    // allSheet[allSheet.length - 1].classList.add('active');
     
-})
-document.querySelector()
+    newSheet.addEventListener('click',handleClick);
+});
+
+function setLastActive(allSheet){
+    for(let i = 0; i < allSheet.length; i++){
+        allSheet[i].classList.remove("active");
+    }
+    allSheet[allSheet.length - 1].classList.add('active');
+}
+
+function handleClick(e){
+    console.log(e.target);
+    let sheet = e.target;
+    let allSheet = document.querySelectorAll('.sheet');
+    for(let i = 0; i < allSheet.length; i++){
+        allSheet[i].classList.remove("active");
+    }
+    sheet.classList.add('active');
+}
+
+// let sL = document.querySelector(".sheet-list");
+// sL.addEventListener('click',function(e){
+//     console.log(e.target);
+//     let allSheet = document.querySelectorAll('.sheet');
+//     for(let i = 0; i < allSheet.length; i++){
+//         allSheet[i].classList.remove("active");
+//     }
+//     e.target.classList.add("active");
+
+// })
