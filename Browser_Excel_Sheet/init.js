1064ff2aab1a78fed2c955a01f8a8b4af2e01806
sanjeevan_rawat <sanjeevanrawat1199@gmail.com>
2021-05-17 11:22:50 +0530

    17 may

--- Browser_Excel_Sheet/init.js
+++ Browser_Excel_Sheet/init.js
@@ -9,20 +9,30 @@ for(let i = 0; i < columns; i++){
     div.classList.add('cell');
     topRow.appendChild(div);
 }
+
 for(let i = 0; i < row; i++){
     let div = document.createElement('div');
     div.innerText = i + 1;
     div.classList.add('block');
     leftCol.appendChild(div);
 }
-for(let i = 0; i < row; i++){
+
+for(let i = 1; i <= row; i++){
     let row = document.createElement('div');
     row.setAttribute('class', 'row');
     for(let j = 0; j < columns; j++){
         let div = document.createElement('div');
-        div.innerText = `${i} ${String.fromCharCode(65 + j)}` ;
+        div.innerText = `${String.fromCharCode(65 + j)} ${i}` ;
         div.classList.add('cell');
         row.appendChild(div);
     }
     grid.appendChild(row);
 }
+
+grid.addEventListener('click', function(e){
+    let content = e.target.innerText;
+    let address = document.querySelector('.address');
+    address.innerText = content;
+    console.log(address);
+
+})
