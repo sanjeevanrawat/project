af08af5015b0b7b83569c98f145b9572cefd8dd8
sanjeevan_rawat <sanjeevanrawat1199@gmail.com>
2021-05-17 10:31:05 +0530

    17 may

---
+++ Browser_Excel_Sheet/init.js
@@ -0,0 +1,28 @@
+let topRow = document.querySelector('.top-row');
+let leftCol = document.querySelector('.left-col');
+let grid = document.querySelector('.grid');
+let columns = 26;
+let row = 100;
+for(let i = 0; i < columns; i++){
+    let div = document.createElement('div');
+    div.innerText = String.fromCharCode(65 + i);
+    div.classList.add('cell');
+    topRow.appendChild(div);
+}
+for(let i = 0; i < row; i++){
+    let div = document.createElement('div');
+    div.innerText = i + 1;
+    div.classList.add('block');
+    leftCol.appendChild(div);
+}
+for(let i = 0; i < row; i++){
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    for(let j = 0; j < columns; j++){
+        let div = document.createElement('div');
+        div.innerText = `${i} ${String.fromCharCode(65 + j)}` ;
+        div.classList.add('cell');
+        row.appendChild(div);
+    }
+    grid.appendChild(row);
+}
