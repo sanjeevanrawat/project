cce30e1fda662365ab2c225469a515d0b5f44790
sanjeevan_rawat <sanjeevanrawat1199@gmail.com>
2021-05-20 16:39:01 +0530

    formula part done

--- Browser_Excel_Sheet/formula.js
+++ Browser_Excel_Sheet/formula.js
@@ -4,29 +4,64 @@ for(let i = 0; i < allCells.length; i++) {
         /*gives rid and cid of element ,in which we are before 
         ex-{ (0,1) after clicking on somewhere else(lets say (1,0)) it gives rid = 0, cid = 1}*/
         let cellObj = sheetArr[rid][cid];
+        if(cellObj.value == allCells[i].innerText){
+            return;
+        }
         cellObj.value = allCells[i].innerText;
         // console.log(cellObj.value);
         //it updates all of its children value related to this cell
         updateChildren(cellObj);
+        if(cellObj.formula){
+            removeFormula(addressElem.value, cellObj, cellObj.formula);
+        }
     })
 }
 
-//1. we added event listener to formulaBar
+//we added event listener to formulaBar
 formulaBar.addEventListener('keydown', function(e){
     if(e.key == 'Enter' && formulaBar.value != ''){
-
-//2. get formula from formulaBar & formula should have space b/w them
-    let cFormula = formulaBar.value;
-//3. getting evaluated value using formula
-    let val = evaluateFormula(cFormula);
-
-//4. setting value in ui(sheet) 
-    let{rid, cid} = getRidCidFromAddress();
-    setCell(val , rid, cid, cFormula);
-    //adds children to parent cell using formula
-    setFormula(cFormula, addressElem.value);
+        let{rid, cid} = getRidCidFromAddress();
+        //get formula from formulaBar & formula should have space b/w them
+        let cFormula = formulaBar.value;
+        let cellObj = sheetArr[rid][cid];
+        if(cellObj.formula == cFormula){
+            return;
+        }
+        //UPDATING THE FORMULA. first we remove old formula
+        if(cellObj.formula){
+            removeFormula(addressElem.value, cellObj, cellObj.formula);
+        }
+        //getting evaluated value using formula
+        let val = evaluateFormula(cFormula);
+        //setting value in ui(sheet)  
+        setCell(val , rid, cid, cFormula);
+        //adds children to parent cell using formula
+        setFormula(cFormula, addressElem.value);
 }
 })
+
+function removeFormula(myName, cellObj, cFormula){
+    let formulaTokens = cFormula.split(' ');
+    for(let i = 0; i < formulaTokens.length; i++){
+        let ascii = formulaTokens[i].charCodeAt(0);
+        //checking if first character of any element in array is an alphabet
+        if(ascii >= 65 && ascii <= 90){
+            //to getting its value,
+            //1. get its rid and cid, (ex-> formulaTokens[i] = A1) 
+            let parentCell = formulaTokens[i];
+            let pCid = Number(parentCell.charCodeAt(0)) - 65;
+            let pRid = Number(parentCell.slice(1)) - 1;
+            // console.log(pRid, pCid);
+            //2.get its value from sheetArr
+            let parentObj = sheetArr[pRid][pCid];
+            let idx = parentObj.children.indexOf(myName);
+            // console.log(pValue);
+            //setting its val in formulaTokens (replacing: A1 -> 10(value of A1 cell))
+            parentObj.children.splice(idx, 1);
+        }
+    }
+    cellObj.formula = "";
+}
 
 function updateChildren(cellObj){
     //takes cellObj and extract children array from it
@@ -70,7 +105,7 @@ function evaluateFormula(cFormula){
     }
     //making it String again (Before-> 2 * A1 : Now-> 2 * 10) 
     let finalFormula = formulaTokens.join(" ");
-    // console.log(finalFormula);
+    console.log(finalFormula);
     //this'll evaluate formula
     return eval(finalFormula);
 }
